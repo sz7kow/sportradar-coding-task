44f5faaceb0d08aeb0c879dfe0a8c307e04582a1
Szymon Kowalski <sz7kow@gmail.com>
2023-01-19 09:10:56 +0000

    style: run prettier

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
 import {Html, Head, Main, NextScript} from 'next/document';
 
-const Document: React.FC = () => <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+const Document: React.FC = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
 export default Document;
