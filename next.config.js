1b7fd62081cec7b1bc083058e28f9846aa94751e
Szymon Kowalski <sz7kow@gmail.com>
2023-01-20 14:26:01 +0000

    progress(web): implement season filtering

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@
 /** @type {NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    /**
+     * The build process has to be throttled to ensure that Queries Per Second limit
+     * of SportRadar's API is not exceeded.
+     */
+    cpus: 1,
+  },
 };
 
 module.exports = nextConfig;
