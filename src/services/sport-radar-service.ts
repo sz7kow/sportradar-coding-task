8187384d91ce5bffbd6e0619d172b4a7e79915fa
Szymon Kowalski <sz7kow@gmail.com>
2023-01-20 14:24:05 +0000

    feat(web): create sport radar service

--- src/services/sport-radar-service.ts
+++ src/services/sport-radar-service.ts
@@ -1,11 +1,61 @@
+import {Season} from '@/types/vendors/sport-radar/season';
 import {SeasonSchedule} from '@/types/vendors/sport-radar/season-schedule';
+import {sleep} from '@/utils/time';
 
-const API_KEY = process.env.SPORT_RADAR_API_KEY;
+export class SportRadarService {
+  static API_KEY = process.env.SPORT_RADAR_API_KEY;
+  static DELAY_BETWEEN_CALLS = 1000;
+  static URL_BASE = 'https://api.sportradar.us/soccer/trial/v4/en/';
 
-export const getSeasonSchedule = async (seasonId: string): Promise<SeasonSchedule[]> => {
-  const scheduleUrl = `https://api.sportradar.us/soccer/trial/v4/en/seasons/${seasonId}/schedules.json?api_key=${API_KEY}`;
-  const response = await fetch(scheduleUrl);
-  const body = await response.json();
+  cache: Map<string, unknown>;
+  queue: Array<string>;
+  isLoading: boolean;
 
-  return body.schedules;
-};
+  constructor() {
+    this.cache = new Map();
+    this.queue = [];
+    this.isLoading = false;
+  }
+
+  async makeRequest<T>(url: string, bodyTransformer: (data: any) => T): Promise<T> {
+    if (this.cache.has(url)) {
+      return this.cache.get(url) as T;
+    }
+
+    this.queue.push(url);
+
+    while (!this.cache.has(url)) {
+      if (this.queue[0] === url && !this.isLoading) {
+        this.isLoading = true;
+
+        const fullUrl = `${SportRadarService.URL_BASE}${url}?api_key=${SportRadarService.API_KEY}`;
+        const response = await fetch(fullUrl);
+
+        if (response.status !== 200) {
+          const responseText = await response.text();
+          throw new Error(`Resuests failed: Status ${response.status} - ${responseText}`);
+        }
+
+        const body = await response.json();
+
+        this.cache.set(url, bodyTransformer(body));
+        this.isLoading = false;
+        this.queue = this.queue.filter((currentUrl) => currentUrl !== url);
+      }
+
+      await sleep(SportRadarService.DELAY_BETWEEN_CALLS);
+    }
+
+    return this.cache.get(url) as T;
+  }
+
+  async getSeasonSchedule(seasonId: string): Promise<SeasonSchedule[]> {
+    return this.makeRequest(`seasons/${seasonId}/schedules.json`, (body) => body.schedules);
+  }
+
+  async getCompetitionSeasons(competitionId: string): Promise<Season[]> {
+    return this.makeRequest(`competitions/${competitionId}/seasons.json`, (body) => body.seasons);
+  }
+}
+
+export const sportRadarService = new SportRadarService();
