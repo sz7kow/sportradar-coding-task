3b2306aeecc363f9ae4a19bba92373ecd6141781
Szymon Kowalski <sz7kow@gmail.com>
2023-01-23 04:01:50 +0000

    feat(server): create sport event prediction service

---
+++ src/services/sport-event-prediction-service.ts
@@ -0,0 +1,28 @@
+import data from '@/data/events.json';
+import {SportEventPrediction} from '@/types/models/sport-event-prediction';
+
+export class SportEventPredictionService {
+  #sportEventPredictions: SportEventPrediction[];
+
+  constructor(sportEventPredictions: SportEventPrediction[]) {
+    this.#sportEventPredictions = sportEventPredictions;
+  }
+
+  getCompatitorNames(): string[] {
+    return Array.from(SportEventPredictionService.#getUniqueCompetitorNames(this.#sportEventPredictions)).sort();
+  }
+
+  static #getUniqueCompetitorNames(sportEventPredictions: SportEventPrediction[]): string[] {
+    const competitorNames = new Set<string>();
+
+    sportEventPredictions.forEach(({competitors}) => {
+      competitors.forEach(({name}) => {
+        competitorNames.add(name);
+      });
+    });
+
+    return Array.from(competitorNames);
+  }
+}
+
+export const sportEventPredictionService = new SportEventPredictionService(data.events as SportEventPrediction[]);
