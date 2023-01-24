1abea19aa83c62d77d9ec63c9974b0897dae1665
Szymon Kowalski <sz7kow@gmail.com>
2023-01-24 01:09:06 +0000

    feat(server): create map to competitor basic utility

---
+++ src/utils/competitor.ts
@@ -0,0 +1,10 @@
+import {CompetitorBasic} from '@/types/models/competitor-basic';
+import {Competitor} from '@/types/vendors/sport-radar/competitor';
+import {CompetitorQualifierEnum} from '@/types/vendors/sport-radar/competitor-qualifier-enum';
+
+export const mapToCompatitorBasic = ({country, id, name, qualifier}: Competitor): CompetitorBasic => ({
+  country: country as string,
+  id,
+  name,
+  qualifier: qualifier as CompetitorQualifierEnum,
+});
