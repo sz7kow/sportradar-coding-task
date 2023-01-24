cfe39cec347446ce29d9f63e89036179fd5525e6
Szymon Kowalski <sz7kow@gmail.com>
2023-01-24 01:12:31 +0000

    feat(server): create map to sport event prediction basic utility

--- src/utils/sport-event-prediction.ts
+++ src/utils/sport-event-prediction.ts
@@ -1,4 +1,10 @@
+import {HighestProbableResultEnum} from '@/types/models/highest-probable-result-enum';
 import {SportEventPrediction} from '@/types/models/sport-event-prediction';
+import {SportEventPredictionBasic} from '@/types/models/sport-event-prediction-basic';
+import {Competitor} from '@/types/vendors/sport-radar/competitor';
+import {CompetitorQualifierEnum} from '@/types/vendors/sport-radar/competitor-qualifier-enum';
+
+import {mapToCompatitorBasic} from './competitor';
 
 export const getUniqueCompetitorNames = (sportEventPredictions: SportEventPrediction[]): string[] => {
   const competitorNames = new Set<string>();
@@ -17,4 +23,45 @@ export const sortDescendingByTheMostProbableResult = (a: SportEventPrediction, b
   const bValue = Math.max(b.probability_away_team_winner, b.probability_draw, b.probability_home_team_winner);
 
   return bValue - aValue;
+};
+
+export const mapToSportEventPredicionBasic = ({
+  competition_id,
+  competition_name,
+  competitors,
+  probability_away_team_winner,
+  probability_draw,
+  probability_home_team_winner,
+  start_date,
+  venue: {name},
+}: SportEventPrediction): SportEventPredictionBasic => {
+  const homeCompetitor = competitors?.find(({qualifier}) => qualifier === CompetitorQualifierEnum.HOME) as Competitor;
+  const awayCompetitor = competitors?.find(({qualifier}) => qualifier === CompetitorQualifierEnum.AWAY) as Competitor;
+
+  const highestProbableResultValue = Math.max(
+    probability_away_team_winner,
+    probability_draw,
+    probability_home_team_winner
+  );
+
+  let highestProbableResult = HighestProbableResultEnum.DRAW;
+
+  if (highestProbableResultValue === probability_home_team_winner) {
+    highestProbableResult = HighestProbableResultEnum.HOME_TEAM_WIN;
+  }
+
+  if (highestProbableResultValue === probability_away_team_winner) {
+    highestProbableResult = HighestProbableResultEnum.HOME_TEAM_WIN;
+  }
+
+  return {
+    away_competitor: mapToCompatitorBasic(awayCompetitor),
+    competition_id,
+    competition_name,
+    highest_probable_result: highestProbableResult,
+    highest_probable_result_value: highestProbableResultValue,
+    home_competitor: mapToCompatitorBasic(homeCompetitor),
+    start_date,
+    venue_name: name,
+  };
 };
