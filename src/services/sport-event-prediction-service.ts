0f8a0a056522eb4a71ed24f31a032ed3922533f6
Szymon Kowalski <sz7kow@gmail.com>
2023-01-24 01:24:16 +0000

    feat(server): create get the most probable results method

--- src/services/sport-event-prediction-service.ts
+++ src/services/sport-event-prediction-service.ts
@@ -1,12 +1,24 @@
 import data from '@/data/events.json';
 import {SportEventPrediction} from '@/types/models/sport-event-prediction';
-import {getUniqueCompetitorNames} from '@/utils/sport-event-prediction';
+import {SportEventPredictionBasic} from '@/types/models/sport-event-prediction-basic';
+import {
+  getUniqueCompetitorNames,
+  mapToSportEventPredicionBasic,
+  sortDescendingByTheMostProbableResult,
+} from '@/utils/sport-event-prediction';
 
 export class SportEventPredictionService {
   #sportEventPredictions: SportEventPrediction[];
 
   constructor(sportEventPredictions: SportEventPrediction[]) {
     this.#sportEventPredictions = sportEventPredictions;
+  }
+
+  getTheMostProbableResults(count: number): SportEventPredictionBasic[] {
+    return this.#sportEventPredictions
+      .sort(sortDescendingByTheMostProbableResult)
+      .slice(0, count)
+      .map(mapToSportEventPredicionBasic);
   }
 
   getCompatitorNames(): string[] {
