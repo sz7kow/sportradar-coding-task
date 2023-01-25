aba9ea60a2f587528951838a8d545cbea4daaadc
sz7kow <sz7kow@gmail.com>
2023-01-25 09:34:58 +0100

    feat(web): modify container spacing

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,5 +3,5 @@ import clsx from 'clsx';
 type DivProps = JSX.IntrinsicElements['div'];
 
 export const Container: React.FC<DivProps> = ({className, ...props}) => (
-  <div {...props} className={clsx(className, 'container mx-auto p-4 xl:max-w-screen-lg')} />
+  <div {...props} className={clsx(className, 'container mx-auto max-w-full px-4 xl:max-w-screen-lg')} />
 );
