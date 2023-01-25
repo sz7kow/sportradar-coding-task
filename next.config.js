66465fa61090c2cf042cdd6e65fd510dac227490
sz7kow <sz7kow@gmail.com>
2023-01-25 05:37:14 +0100

    chore: configure @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
      */
     cpus: 1,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
